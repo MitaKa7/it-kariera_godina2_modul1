037635f4b38f1c9f19bd7a682895f2e0e79ccf16
MitaKa7 <74647734+MitaKa7@users.noreply.github.com>
2024-09-17 19:33:04 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPStatistics
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OOPStatistics;
+
+int peopleCount = int.Parse(Console.ReadLine());
+
+List<Person> people = new List<Person>();
+
+for (int i = 0; i < peopleCount; i++)
+{
+    string[] data = Console.ReadLine().Split();
+
+    people.Add(new Person(name: data[0], age: int.Parse(data[1])));
+}
+
+people = people.Where(person => person.Age > 30).OrderBy(person => person.Name).ToList();
+
+Console.WriteLine(string.Join("\n", people));
