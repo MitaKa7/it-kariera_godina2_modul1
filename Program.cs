3b819a12ab41f9f0f7e6d7766c0277aa8fdb3720
MitaKa7 <74647734+MitaKa7@users.noreply.github.com>
2024-09-21 10:56:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using InheritanceExample;
+
+Person person = new Person("Dimitar", 17);
+
+Console.WriteLine(person);
+
+Employee employee = new Employee("Dimitar", 17, "Bulgarian Coffee Services");
+Console.WriteLine(employee);
+
+Student student = new Student("Dimitar", 17, "NEG-GOETHE");
+Console.WriteLine(student);
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public class Student : Person
+    {
+		private string school;
+
+		public string School
+		{
+			get { return school; }
+			set { school = value; }
+		}
+
+        public Student(string name, int age, string school) : base(name, age)
+        {
+            School = school;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}, school: {School} "; //втори начин за писане на ToString() метода
+        }
+    }
+}
