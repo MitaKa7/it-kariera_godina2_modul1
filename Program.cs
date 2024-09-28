4877e529689762e7d165c19a18cfe1ea758499c5
MitaKa7 <74647734+MitaKa7@users.noreply.github.com>
2024-09-28 14:35:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using LibraryIteration;
+
+Library library = new Library();
+library.Add(new Book("Prikazka za stulbata", 1900, " Hristo Smirnenski"));
+library.Add(new Book("Knigata na shefa", 2024, "Author 3", "Author 4"));
+library.Add(new Book("How to cook with Author 5 and 6", 2024, "Author 5", "Author 6"));
+library.Add(new Book("The biography of the King - Pele", 2024, "Author 7", "Author 8"));
+
+foreach (Book book in library)
+{
+    Console.WriteLine(book);
+}
+
