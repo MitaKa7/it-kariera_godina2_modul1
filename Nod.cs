a3bf32f0076143da2cc3911ebbeec7a0942a0e91
MitaKa7 <74647734+MitaKa7@users.noreply.github.com>
2024-09-28 15:06:26 +0300

    Add files via upload

---
+++ Nod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack2024
+{
+    public class Node<T>
+    {
+        public T Element { get; set; }   
+        
+        public Node<T> Next { get; set; }
+
+        public Node(T element) 
+        {
+            Element = element;
+            Next = null;
+        }
+
+
+
+    }
+}
