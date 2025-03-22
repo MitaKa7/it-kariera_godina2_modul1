2ab0d57358636ade1a6698a9892cfbfce1ed0305
MitaKa7 <74647734+MitaKa7@users.noreply.github.com>
2025-03-22 14:51:37 +0200

    Add files via upload

---
+++ OCR demo-Programm.cs
@@ -0,0 +1,21 @@
+﻿using Tesseract;
+
+try
+{
+    string imageName = "test.png";
+    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+    {
+        using (var img = Pix.LoadFromFile(imageName))
+        {
+            using (var page = engine.Process(img))
+            {
+                string text = page.GetText();
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
